32ae0e7e15fad502105dd9456139382d5c430c2b
vahanmetric1 <vahan@metric.am>
2021-06-02 18:24:34 +0400

    spacings text ui changes

--- js/main.js
+++ js/main.js
@@ -1,16 +1,76 @@
 const submitButton = document.getElementById('submit-button');
-const form = document.getElementById('mail-form');
+const backButton = jQuery('#backButton');
+const okButton = jQuery('#okButton');
+const form = jQuery('#mail-form');
 const formParent = form.parentNode;
 let isButtonDisabled = false;
+let yourName = jQuery('#your-name');
+let message = jQuery('#your-message');
+let subject = jQuery('#your-subject');
+let email = jQuery('#your-email');
+
+const handleBackOkClick = () => {
+  const successContainer = jQuery('#mail-send-success');
+  const errorContainer = jQuery('#mail-send-success');
+  successContainer.hide();
+  errorContainer.hide();
+  yourName.val('');
+  message.val('');
+  subject.val('');
+  email.val('');
+  form.show();
+}
+
+okButton.click(handleBackOkClick)
+backButton.click(handleBackOkClick)
+
+yourName.keydown(event => {
+  yourName.css('border', 'none');
+})
+
+message.keydown(event => {
+  message.css('border', 'none');
+})
+
+subject.keydown(event => {
+  subject.css('border', 'none');
+})
+
+email.keydown(event => {
+  email.css('border', 'none');
+})
+
 
 submitButton.addEventListener('click', async () => {
     if (isButtonDisabled) {
         return;
     }
-    const name = document.getElementById('your-name');
-    const message = document.getElementById('your-message');
-    const subject = document.getElementById('your-subject');
-    const email = document.getElementById('your-email');
+    
+    yourName = jQuery('#your-name');
+    message = jQuery('#your-message');
+    subject = jQuery('#your-subject');
+    email = jQuery('#your-email');
+    let isError = false;
+    if (!yourName.val()) {
+      yourName.css("border", "1px solid red");
+      isError = true;
+    }
+    if (!message.val()) {
+      message.css("border", "1px solid red");
+      isError = true;
+    }
+    if (!subject.val()) {
+      subject.css("border", "1px solid red");
+      isError = true;
+    }
+    if (!email.val()) {
+      email.css("border", "1px solid red");
+      isError = true;
+    }
+    if (isError) {
+      jQuery('#send-mail-error').show();
+      return;
+    }
     isButtonDisabled = true;
 
     const response = await fetch('https://api.emailjs.com/api/v1.0/email/send', {
@@ -23,66 +83,23 @@ submitButton.addEventListener('click', async () => {
             template_id: 'template_ne9iy4e',
             user_id: 'user_iEyfJ0OmIwtGmVBh36X4J',
             template_params: {
-                name: name.value,
-                email: email.value,
-                message: message.value,
-                subject: subject.value,
+                name: yourName.val(),
+                email: email.val(),
+                message: message.val(),
+                subject: subject.val(),
                 fromWebsite: 'Pinsight'
             }
         }) // body data type must match "Content-Type" header
       });
-      if (response.ok) {
-        formParent.removeChild(form);
-        const div = document.createElement('div');
-        div.style.display = 'flex';
-        div.style.flexDirection = 'column';
-        div.style.height = '200px';
-        div.style.width = '100%';
-        div.style.alignItems = 'center';
-        const titleSpan = document.createElement('span');
-        titleSpan.className = 'resultTitle';
-        titleSpan.innerHTML = 'Thank You!'
-        const textSpan = document.createElement('span');
-        textSpan.innerHTML = 'We have received your email we will contact you as soon as possible.'
-        textSpan.className = 'resultText';
-        const okButton = document.createElement('button');
-        okButton.innerHTML = 'OK'
-        okButton.className = 'okButton bg-purple';
-        okButton.addEventListener('click', () => {
-          formParent.removeChild(div);
-          formParent.appendChild(form);
-        })
-        div.appendChild(titleSpan);
-        div.appendChild(textSpan);
-        div.appendChild(okButton);
-        formParent.appendChild(div);
+      if (response && response.ok) {
+        const successContainer = jQuery('#mail-send-success');
+        form.hide();
+        successContainer.css('display', 'flex');
         isButtonDisabled = false;
         return;
       }
-      formParent.removeChild(form);
-      const div = document.createElement('div');
-      div.style.display = 'flex';
-      div.style.flexDirection = 'column';
-      div.style.height = '200px';
-      div.style.width = '100%';
-      div.style.alignItems = 'center';
-      const titleSpan = document.createElement('span');
-      titleSpan.className = 'resultTitle';
-      titleSpan.innerHTML = 'Error!'
-      const textSpan = document.createElement('span');
-      textSpan.innerHTML = 'An internal error occurred during your request, please try again.'
-      textSpan.className = 'resultText';
-      const okButton = document.createElement('button');
-      okButton.innerHTML = 'OK'
-      okButton.className = 'okButton bg-orange';
-      okButton.addEventListener('click', () => {
-        formParent.removeChild(div);
-        formParent.appendChild(form);
-      })
-      div.appendChild(titleSpan);
-      div.appendChild(textSpan);
-      div.appendChild(okButton);
-      formParent.appendChild(div);
-      formParent.appendChild(span);
+      const errorContainer = jQuery('#mail-send-success');
+      form.hide();
+      errorContainer.css('display', 'flex');
       isButtonDisabled = false;
 })
